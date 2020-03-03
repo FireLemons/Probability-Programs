91ec1062b6c96ce8bdcd4bff83ef88bb30dde6b9
firelemons <shenyang64@gmail.com>
2020-03-03 13:52:24 -0600

    frequency data conversion function handles only numeric lists

--- book_programs/chapter_1/h_t_simulation.py
+++ book_programs/chapter_1/h_t_simulation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 # Usage: python3 de_mere_2.py <m> <n> <mute_output>
-# Param m:              The number of coin flips in each experiment
-# Param n:              The number of coin flip experiments to run
+#   m:              The number of coin flips in each experiment
+#   n:              The number of coin flip experiments to run
 #                       If n is set to 1 the winnings will be plotted
 #                       If n is greater than 1 the distributions of overall winnings and times in the lead will be plotted
-# Param mute_output:    If the value is "mute" or "m" the results of the experiments will not be plotted
+#   mute_output:    If the value is "mute" or "m" the results of the experiments will not be plotted
 
 # Example Usage: python3 h_t_simulation.py
 
@@ -32,7 +32,7 @@ except IndexError:
     pass
 except ValueError:
     bad_input = True
-    print("ERROR: Could not convert param n to an integer")
+    print("ERROR: Could not convert n to an integer")
 
 try:
     random_coin_experiment_count = int(args[2])
@@ -40,17 +40,17 @@ except IndexError:
     pass
 except ValueError:
     bad_input = True
-    print("ERROR: Could not convert param m to an integer")
+    print("ERROR: Could not convert m to an integer")
 
 # Argument n is negtive
 if 'random_coin_experiment_count' in globals() and random_coin_experiment_count < 0:
     bad_input = True
-    print("ERROR: Param n cannot be negative")
+    print("ERROR: n cannot be negative")
 
 # Argument m is negtive
 if 'coin_flip_count' in globals() and coin_flip_count < 0:
     bad_input = True
-    print("ERROR: Param m cannot be negative")
+    print("ERROR: m cannot be negative")
 
 # Print usage help when arguments are bad
 if bad_input:
@@ -96,23 +96,32 @@ for i in range(random_coin_experiment_count):
 
 # Functions to reformat data for plotting
  
-def list_to_frequency_list(recurring_element_list):
+def number_list_to_frequency_list(recurring_number_list):
     """
     Converts a list of numbers into a frequency representation
 
         Args:
-            recurring_element_list (list): A list
+            recurring_number_list (list): A list of numbers
 
         Returns:
             dict: A dictionary where the keys are values from number_list and the values are how frequently they appear in number_list
+
+        Raises:
+           TypeError: If recurring_number_list contains a non numeric value
     """
     frequency_table = {}
 
-    for elem in recurring_element_list:
+    for elem in recurring_number_list:
         if elem in frequency_table:
             frequency_table[elem] += 1
         else:
             frequency_table[elem] = 1
+
+    unique_elements = frequency_table.keys()
+
+    for elem in unique_elements:
+        if not(isinstance(elem, float) or isinstance(elem, int)):
+            raise TypeError("ERROR: An element of recurring_number_list was not numeric")
 
     return frequency_table
 
@@ -131,7 +140,7 @@ else:
     pyplot.figure()
     #Plot figure 1.2
     #Plot figure 1.3
-    frequency_data = list_to_frequency_list(times_in_lead_list)
+    frequency_data = number_list_to_frequency_list(times_in_lead_list)
     pyplot.subplot(212)
     pyplot.suptitle("Figure 1.3: Distribution of number of times in the lead.")
     pyplot.stem(frequency_data.keys(), frequency_data.values())
