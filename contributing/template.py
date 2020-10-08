37f2b807f0b7da773abe2e108497fc3f13fe1865
firelemons <shenyang64@gmail.com>
2020-10-08 18:46:52 -0500

    added contributing folder with mathematica files and template

---
+++ contributing/template.py
@@ -0,0 +1,38 @@
+# See existing scripts in ../book_programs/chapter_1/ for examples
+
+# USAGE HINT
+# Usage: python3 name_of_program.py <arg1> <arg2>
+#   arg1:   arg1 description
+#   arg2:   arg2 description
+
+# Example Usage: python3 name_of_program.py 30 foo
+
+#import matplotlib.pyplot as pyplot for graphing
+import random
+import sys
+
+args = sys.argv
+
+#####################
+# User Input Handling
+#####################
+bad_input = False
+
+# User input error checking here
+
+# Print usage help when arguments are bad
+if bad_input:
+    print("\nUSAGE HINT from above")
+    sys.exit(1)
+
+###################
+# Name of Program #
+###################
+
+# Program code
+
+##################
+# Graphing Results
+##################
+
+# Print Results
