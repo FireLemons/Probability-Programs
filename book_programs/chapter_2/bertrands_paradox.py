ec97b88ca1799e0a8519e61fd157e25d4ebc4fb0
VPraharsha03 <56556436+VPraharsha03@users.noreply.github.com>
2020-10-16 16:08:44 +0000

    Update usage

--- book_programs/chapter_2/bertrands_paradox.py
+++ book_programs/chapter_2/bertrands_paradox.py
@@ -1,8 +1,8 @@
-#	Usage: python3 bertrands_paradox.py <NCHORDS_TO_PLOT> <nchords> 
-#	NCHORDS_TO_PLOT:  The number of chords to plot
-#	Don't plot more than 1000 number of chords because they overlap too much
-#	and obscure the point we're trying to make.
-#	nchords:  The sample size of chords to do the statistics
+#   Usage: python3 bertrands_paradox.py <NCHORDS_TO_PLOT> <nchords> 
+#     NCHORDS_TO_PLOT:  The number of chords to plot
+#       Don't plot more than 1000 number of chords because they overlap too much 
+#       and obscure the point we're trying to make.
+#     nchords:  The sample size of chords to do the statistics
 
 # Example Usage: python3 bertrands_paradox.py 1000 10000
 
