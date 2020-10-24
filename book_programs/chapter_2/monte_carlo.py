c771e9fbc2c804f03a4f3c87957ce701bece2e31
tincher <joel.ewig@gmail.com>
2020-10-24 13:26:21 +0200

    monte_carlo

--- book_programs/chapter_2/monte_carlo.py
+++ book_programs/chapter_2/monte_carlo.py
@@ -1,40 +1,50 @@
-# Usage: python3 monte_carlo.py <n> <f> <xmin> <xmax> <ymax>
+# Usage: python3 monte_carlo.py <n> <xmin> <xmax> <ymax>
 #   n:      number of random numbers
-#   f:      function to be inspected
 #   xmin:   start of interval in which random numbers are generated
 #   xmax:   end of interval in which random numbers are generated
 #   ymax:   largest y value to be considered
-
-# Example Usage: python3 monte_carlo.py 100 cos 0 1 1
+#
+#   f:      function to be inspected - defined in the python file
+#
+# Example Usage: python3 monte_carlo.py 100 0 1 1
 
 import random
 import sys
 import matplotlib.pyplot as pyplot
 import numpy as np
 
-# TODO
 import math
 
 
 args = sys.argv
 
+
+#####################
+# User Input
+#####################
+
+def inspected_function(x):
+    return math.cos(x)
+
+
 #####################
 # User Input Handling
 #####################
+
+
 bad_input = False
 
 # No arguments
-if len(args) < 6:
+if len(args) < 5:
     bad_input = True
     print("ERROR: Insufficient arguments")
 
 # Check if the function is defined
 try:
-    function = getattr(math, args[2])
-    x_min = float(args[3])
-    x_max = float(args[4])
-    y_max = float(args[5])
-    function(x_min)
+    x_min = float(args[2])
+    x_max = float(args[3])
+    y_max = float(args[4])
+    inspected_function(x_min)
 except:
     bad_input = True
     print("ERROR: f is not a valid function - could not solve for xmin")
@@ -68,11 +78,11 @@ if 'random_numbers_count' in globals() and random_numbers_count < 0:
 if bad_input:
     print("\nUsage: python3 monte_carlo.py <n> <f> <xmin> <xmax> <ymax>")
     print("  n:      number of random numbers")
-    print("  f:      function to be inspected")
     print("  xmin:   start of interval in which random numbers are generated")
     print("  xmax:   end of interval in which random numbers are generated")
     print("  ymax:   largest y value to be considered")
-    print("\nExample Usage: python3 monte_carlo.py 100 cos 0 1 1")
+    print("  f:      function to be inspected - defined in the python file")
+    print("\nExample Usage: python3 monte_carlo.py 100 0 1 1")
 
     sys.exit(1)
 
@@ -85,7 +95,7 @@ points_under_function_count = 0
 
 for i in range(random_numbers_count):
     point = {'x': random.uniform(x_min, x_max), 'y': random.uniform(0, y_max)}
-    if point['y'] <= function(point['x']):
+    if point['y'] <= inspected_function(point['x']):
         points_under_function_count += 1
     points.append(point)
 
@@ -100,8 +110,9 @@ print("Area Estimate: ", round(area_estimate, 3))
 random_xs = [point['x'] for point in points]
 random_ys = [point['y'] for point in points]
 
-function_xs = np.arange(x_min, x_max, (x_max - x_min) / 20)
-function_ys = [function(x) for x in function_xs]
+increment = (x_max - x_min) / 20
+function_xs = np.arange(x_min, x_max + increment, increment)
+function_ys = [inspected_function(x) for x in function_xs]
 
 
 pyplot.suptitle("1.1 Function and generated Points")
